838ef3c27d933618215415463ba9d6e0fecac46b
Cedeno Mark bryan <cedenomarkbryan04@gmail.com>
2025-09-17 23:09:47 +0800

    Fix missing navbar

--- ai-powered-grading-system/frontend/components/navbar.php
+++ ai-powered-grading-system/frontend/components/navbar.php
@@ -1,15 +1,15 @@
 <!-- Navbar -->
-    <header>
-      <nav class="navbar">
-        <div class="logo">
-          <a href="landing.php">
-            <span class="plmun">PLMUN</span>
-            <span class="portal">Portal</span>
-          </a>
-        </div>
-        <ul class="nav-links">
-          <li><a href="login.php" class="btn small outline">Login</a></li>
-          <li><a href="signup.php" class="btn small outline">Sign Up</a></li>
-        </ul>
-      </nav>
-    </header>
+<header>
+  <nav class="navbar">
+    <div class="logo">
+      <a href="landing.php">
+        <span class="plmun">PLMUN</span>
+        <span class="portal">Portal</span>
+      </a>
+    </div>
+    <ul class="nav-links">
+      <li><a href="login.php" class="btn small outline">Login</a></li>
+      <li><a href="signup.php" class="btn small outline">Sign Up</a></li>
+    </ul>
+  </nav>
+</header>
